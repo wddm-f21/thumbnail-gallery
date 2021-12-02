f58db84f6f3561984a9bfcc8464956f9149a8933
Rocco P <roccopan@gmail.com>
2021-12-02 10:14:17 -0500

    getting there

--- js/script.js
+++ js/script.js
@@ -3,20 +3,18 @@ const t2 = document.querySelector(`#thumb2`)
 const t3 = document.querySelector(`#thumb3`)
 const lg = document.querySelector(`#largeimg`)
 
+// Will receive a reference to a thumbnail <img>, set its value to the large <img>
 const setLargeImgSrc = function(thumb) {
   const url = thumb.getAttribute(`src`)
   lg.setAttribute(`src`, url)
 }
 
+// Call setLargeImgSrc with the url from this thumbnail <img> (event.target)
+const handleThumbClick = function(event) {
+  setLargeImgSrc(event.target)
+}
 
-t1.addEventListener(`click`, function() {
-  setLargeImgSrc(t1)
-})
-
-t2.addEventListener(`click`, function() {
-  setLargeImgSrc(t2)
-})
-
-t3.addEventListener(`click`, function() {
-  setLargeImgSrc(t3)
-})
+// Add listeners to each thumbnail
+t1.addEventListener(`click`, handleThumbClick)
+t2.addEventListener(`click`, handleThumbClick)
+t3.addEventListener(`click`, handleThumbClick)

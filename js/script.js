9be64f161d4a078d8f63bc1648a0a89bbac7fdd9
Rocco P <roccopan@gmail.com>
2021-12-16 10:16:01 -0500

    event delegation

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
-const thumbs = document.querySelectorAll(`.thumbnail`)
+const thumbs = document.querySelector(`.thumbs`)
 const lg = document.querySelector(`#largeimg`)
 
 // Call setLargeImgSrc with the url from this thumbnail <img> (event.target)
 // Will receive a reference to a thumbnail <img>, set its value to the large <img>
 const handleThumbClick = function(event) {
   const thumb = event.target
+  console.log(thumb)
+
+  // If what was clicked was not an image: exit immediately!
+  if (!thumb.matches(`img`)) return
+
   const url = thumb.getAttribute(`src`)
   lg.setAttribute(`src`, url)
 }
 
 // Add listeners to each thumbnail
-thumbs.forEach(function(eleThumb, i) {
-  eleThumb.addEventListener(`click`, handleThumbClick)
-})
+thumbs.addEventListener(`click`, handleThumbClick)
 
 // NEXT: Event delegation (to make this EVEN MORE efficient)
